df20dbc5cd8f433063dbbca33ac8544b739224cf
Cameron Hickey <cameronhickey@gmail.com>
2013-04-12 17:05:27 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefugeeRegistry::Application.config.secret_token = '4bf9dd9f0fd9e94413c5ef78edea7cba20565b17917a64f0d6ab52ae0cb0572d0cfeadbf98563d6f5d0120a14f55a13a1f9c3ba1bb6abbb4b734cd9a7a5a6740'
